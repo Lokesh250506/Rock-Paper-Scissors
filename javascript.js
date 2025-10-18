e760e1604bcc8db00f605931d090c9cdcb0b2d77
Lokesh250506 <theredlion321@gmail.com>
2025-10-18 18:58:08 +0000

    WITH UI

--- javascript.js
+++ javascript.js
@@ -20,66 +20,190 @@ let computerscore = 0;
 function playRound (userchoice, computerchoice) {
     if (userchoice === "rock") {
         if(computerchoice === "rock") {
-            alert ("Tie");
+            let a = document.createElement("p");
+            a.textContent = "It's a TIE";
+            let b = document.querySelector ("div");
+            b.textContent = "";
+            b.appendChild(a);
             userscore = userscore + 1;
             computerscore = computerscore + 1;
         }
         else if(computerchoice === "paper") {
-            alert ("You Lose!! Paper beats rock");
+            let a = document.createElement("p");
+            a.textContent = "You Lose!! Paper beats rock";
+            let b = document.querySelector ("div");
+            b.textContent = "";
+            b.appendChild(a);
             computerscore = computerscore + 1;
         }
         else {
-            alert ("You Win!! Rock beats Scissors");
+            let a = document.createElement("p");
+            a.textContent = "You Win!! Rock beats Scissors";
+            let b = document.querySelector ("div");
+            b.textContent = "";
+            b.appendChild(a);
             userscore = userscore + 1;
         }
     }
     else if (userchoice === "paper") {
         if(computerchoice === "paper") {
-            alert ("Tie");
+            let a = document.createElement("p");
+            a.textContent = "It's a TIE";
+            let b = document.querySelector ("div");
+            b.textContent = "";
+            b.appendChild(a);
             userscore = userscore + 1;
             computerscore = computerscore + 1;
         }
         else if(computerchoice === "scissors") {
-            alert ("You Lose!! Scissors beats paper");
+            let a = document.createElement("p");
+            a.textContent = "You Lose!! Scissors beats paper";
+            let b = document.querySelector ("div");
+            b.textContent = "";
+            b.appendChild(a);
             computerscore = computerscore + 1;
         }
         else {
-            alert ("You Win!! Paper beats Rock");
+            let a = document.createElement("p");
+            a.textContent = "You Win!! Paper beats Rock";
+            let b = document.querySelector ("div");
+            b.textContent = "";
+            b.appendChild(a);
             userscore = userscore + 1;
         }
     }
     else {
         if(computerchoice === "scissors") {
-            alert ("Tie");
+            let a = document.createElement("p");
+            a.textContent = "It's a TIE";
+            let b = document.querySelector ("div");
+            b.textContent = "";
+            b.appendChild(a);
             userscore = userscore + 1;
             computerscore = computerscore + 1;
         }
         else if(computerchoice == "rock") {
-            alert ("You Lose!! Rock beats Scissors");
+            let a = document.createElement("p");
+            a.textContent = "You Lose!! Rock beats Scissors";
+            let b = document.querySelector ("div");
+            b.textContent = "";
+            b.appendChild(a);
             computerscore = computerscore + 1;
         }
         else {
-            alert ("You Win!! Scissors beats Paper");
+            let a = document.createElement("p");
+            a.textContent = "You Win!! Scissors beats Paper";
+            let b = document.querySelector ("div");
+            b.textContent = "";
+            b.appendChild(a);
             userscore = userscore + 1;
         }
     }
-}
 
-function playGame () {
-    for (let i = 0; i<5 ; i++) {
-        let computerchoice = getComputerChoice();
-        let humanchoice = getHumanChoice().toLowerCase();
-        playRound (humanchoice, computerchoice);
-    }
-    if (computerscore > userscore ) {
-        alert("Computer wins the game, You are a big loser, Computer score :"+computerscore+" Your Score :"+userscore);
-    }
-    else if (userscore > computerscore) {
-        alert ("Congratz bro, you won, Computer score "+computerscore+" Your Score :"+userscore);
-    }
-    else {
-        alert ("Its a tie kiddo, Computer score :"+computerscore+" Your Score :"+userscore);
+    if (userscore === 5 || computerscore === 5) {
+        gameOver();
     }
 }
 
-playGame();
+function gameOver () {
+    // Disable the buttons to stop play
+    let button1 = document.querySelector (".button1");
+    let button2 = document.querySelector (".button2");
+    let button3 = document.querySelector (".button3");
+
+    // Set the 'disabled' attribute to true
+    button1.disabled = true;
+    button2.disabled = true;
+    button3.disabled = true;
+    
+    // Optionally change the button style to show they are inactive
+    button1.style.backgroundColor = "lightgray";
+    button2.style.backgroundColor = "lightgray";
+    button3.style.backgroundColor = "lightgray";
+}
+
+
+function func1 (e) {
+    let computerchoice = getComputerChoice();
+    userchoice = "rock";
+    playRound (userchoice, computerchoice);
+    if (userscore !=5 && computerscore !=5) {
+    let a = document.createElement ("p");
+    a.textContent = "Your score is: " +userscore+ " Computer score is:"+computerscore;
+    let b = document.querySelector(".div");
+    b.appendChild(a);
+    }
+       else {
+        let b = document.querySelector(".div");
+        b.textContent = "";
+        if (userscore===5){
+            let a = document.createElement("p");
+            a.textContent = "Damnn!! You won, Your score :"+userscore+ "Computer score: "+computerscore;
+            b.appendChild(a);
+        }
+        else if (computerscore ===5) {
+            let a = document.createElement("p");
+            a.textContent = "Awww!! You lost, Your score:"+userscore+ ", Computer score:"+computerscore;
+            b.appendChild(a);
+        }
+    }
+}
+function func2 (e) {
+    let computerchoice = getComputerChoice();
+    userchoice = "paper";
+    playRound (userchoice, computerchoice);
+    if (userscore !=5 && computerscore !=5) {
+    let a = document.createElement ("p");
+    a.textContent = "Your score is: " +userscore+ " Computer score is:"+computerscore;
+    let b = document.querySelector(".div");
+    b.appendChild(a);
+    }
+       else {
+        let b = document.querySelector(".div");
+        b.textContent = "";
+        if (userscore===5){
+            let a = document.createElement("p");
+            a.textContent = "Damnn!! You won, Your score :"+userscore+ "Computer score: "+computerscore;
+            b.appendChild(a);
+        }
+        else if (computerscore ===5) {
+            let a = document.createElement("p");
+            a.textContent = "Awww!! You lost, Your score:"+userscore+ ", Computer score: "+computerscore;
+            b.appendChild(a);
+        }
+    }
+}
+function func3 (e) {
+    let computerchoice = getComputerChoice();
+    userchoice = "scissors";
+    playRound (userchoice, computerchoice);
+    if (userscore !=5 && computerscore !=5) {
+    let a = document.createElement ("p");
+    a.textContent = "Your score is: " +userscore+ " Computer score is:"+computerscore;
+    let b = document.querySelector(".div");
+    b.appendChild(a);
+    }
+       else {
+        let b = document.querySelector(".div");
+        b.textContent = "";
+        if (userscore===5){
+            let a = document.createElement("p");
+            a.textContent = "Damnn!! You won, Your score :"+userscore+ ", Computer score: "+computerscore;
+            b.appendChild(a);
+        }
+        else if (computerscore ===5) {
+            let a = document.createElement("p");
+            a.textContent = "Awww!! You lost, Your score:"+userscore+ ", Computer score: "+computerscore;
+            b.appendChild(a);
+        }
+    }
+}
+let button1 = document.querySelector (".button1");
+button1.addEventListener ("click", func1);
+
+let button2 = document.querySelector (".button2");
+button2.addEventListener ("click", func2);
+
+let button3 = document.querySelector (".button3");
+button3.addEventListener ("click", func3); 
+
